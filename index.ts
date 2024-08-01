844d253a3453b1bdac0e4cac27a72a85062670e0
AizazAnwar11290 <140271798+AizazAnwar11290@users.noreply.github.com>
2024-08-01 14:27:12 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,38 @@
+#! /usbin/env node
+
+import inquirer from "inquirer";
+
+console.log("WelCome to Aizaz Anwar simple-calculator");
+
+const answer = await inquirer.prompt([
+  {
+    message: "Please Enter your first Number : ",
+    type: "number",
+    name: "firstNumber",
+  },
+  {
+    message: "Please Enter your second Number : ",
+    type: "number",
+    name: "secondNumber",
+  },
+  {
+    message: "Please select any off operator to perform action : ",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiply", "Division"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator === "Multiply") {
+  console.log(answer.firstNumber * answer.secondNumber);
+} else if (answer.operator === "Division") {
+  console.log(answer.firstNumber / answer.secondNumber);
+} else {
+  console.log("Invalid operatore selected...");
+}
+
+console.log("THE END");
